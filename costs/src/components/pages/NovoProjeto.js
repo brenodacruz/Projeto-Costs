99b268e9f2b8a10743592f28a30d8f67233ba522
brenodacruz <brenohenriquecruz22@gmail.com>
2024-06-11 16:57:30 -0300

    Banco de dados
    Adicionei o db.json como um banco de dados para o projeto, e vinculei com o formulário.

--- costs/src/components/pages/NovoProjeto.js
+++ costs/src/components/pages/NovoProjeto.js
@@ -1,13 +1,38 @@
+import { useNavigate } from 'react-router-dom'
+
+
 import styles from './NovoProjeto.module.css'
 import Formulario from '../projects/Formulario'
 
 function NovoProjeto(){
+
+    const navigate = useNavigate()
+
+    function createPost(project){
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        }).then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                navigate("/projetos", { message: "Projeto criado com sucesso!" })
+            })
+            .catch(err => console.log(err))
+
+    }
+
     return(
         <div className={styles.container_center}>
             <div className={styles.container}>
                 <h1>Criar Projeto</h1>
                 <p>Crie seu projeto para depois adicionar os serviços</p>
-                <Formulario btnText="Criar Projeto"></Formulario>
+                <Formulario handleSubmit={createPost} btnText="Criar Projeto"></Formulario>
             </div>
         </div>
     )
--- costs/src/components/projects/Formulario.js
+++ costs/src/components/projects/Formulario.js
@@ -1,13 +1,49 @@
+import { useEffect, useState } from 'react'
+
 import Btn from "../form/Btn"
 import Input from "../form/Input"
 import Select from "../form/Select"
 
-function Formulario({btnText}){
-    return(
-        <form>
-            <Input type="text" placeholder="Insira o nome do projeto" name="name" text="Nome do Projeto"></Input>
-            <Input type="number" placeholder="Insira o orçamento total" name="name" text="Orçamento Total"></Input>
-            <Select name="category_id" text="Selecione a categoria"></Select>
+function Formulario({ handleSubmit, btnText, projectData }) {
+    const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+            .then((data) => { setCategories(data) })
+            .catch((err) => console.log(err))
+    }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+       // console.log(project)
+        handleSubmit(project)
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]: e.target.value})
+    }
+
+    function handleCategory(e){
+        setProject({
+            ...project,
+            category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text,
+            },
+        })
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <Input type="text" placeholder="Insira o nome do projeto" name="name" text="Nome do Projeto" handleOnChange={handleChange} value={project.name ? project.name: ''}></Input>
+            <Input type="number" placeholder="Insira o orçamento total" name="budget" text="Orçamento Total" handleOnChange={handleChange} value={project.budget ? project.budget: ''}></Input>
+            <Select name="category_id" text="Selecione a categoria" options={categories} handleOnChange={handleCategory} value={project.category ? project.category.id: ''}></Select>
             <Btn text={btnText}></Btn>
         </form>
     )
